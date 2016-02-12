b9c8d94e008dfe8a4c7f9e93c7e3cd7c3e804f81
Sabri Helal & Marshall Sosland <pair+lechiffre10.msosland@devbootcamp.com>
2016-02-12 17:29:38 -0600

    begin create new deck functionality

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,3 +1,15 @@
 get '/users/:user_id/decks/new' do
-
+  redirect '/' unless params[:user_id] == session[:user_id]
+  erb :'deck/new'
 end
+
+
+post '/users/:user_id/decks/:deck_id/cards' do
+  redirect '/' unless params[:user_id] == session[:user_id]
+
+
+
+
+
+
+post '/users/:user_id/decks' do
