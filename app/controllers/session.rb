6d15354fd112d1d727f7f09975cacd34ccb9d6ae
Sabri Helal & Marshall Sosland <pair+lechiffre10.msosland@devbootcamp.com>
2016-02-13 16:51:22 -0600

    Add user creating decks

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -18,7 +18,7 @@ post '/login/?' do
     session[:user_id] = user.id
     redirect "/user/#{user.id}"
   else
-    @error_login = 'Nope! GUESSARITOGAIN!'
+    @error_login = 'Invalid Username or Password'
     erb :'login'
   end
 end
